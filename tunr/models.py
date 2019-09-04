206ec14e8ec76332f88878b1ce173d68d4e4ada5
“Erin <erinkelley27@gmail.com>
2019-09-04 16:47:58 -0400

    song model and admin file

--- tunr/models.py
+++ tunr/models.py
@@ -7,3 +7,12 @@ class Artist(models.Model):
 
     def __str__(self):
         return self.name
+
+class Song(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
+    title = models.CharField(max_length=100, default='no song title')
+    album = models.CharField(max_length=100, default='no album title')
+    preview_url = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.title
